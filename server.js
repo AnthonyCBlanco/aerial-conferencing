8c0c49c4b1af8098c994969a144de82a98b310d4
Anthony Blanco <anthony200527@outlook.com>
2024-02-07 18:42:09 -0800

    ITS WORKING -comment need to transfer to handlebars template instead of static html

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 const exphbs = require('express-handlebars');
+const PORT = 3001
 
 const hbs = exphbs.create();
 
@@ -14,37 +13,8 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
 
-  res.redirect(`/${uuidV4()}`)
+  res.render('')
 
 })
 
-
-app.get('/:room', (req, res) => {
-
-  res.render('room', { roomId: req.params.room })
-
-})
-
-
-// WebSocket Communication
-
-io.on('connection', socket => {
-
-  socket.on('join-room', (roomId, userId) => {
-
-    socket.join(roomId)
-
-    socket.to(roomId).broadcast.emit('user-connected', userId)
-
-    socket.on('disconnect', () => {
-
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-
-    })
-
-  })
-
-})
-
-// Starting the Server
-server.listen(3000)
+app.listen(PORT, () => console.log('Now listening'));
